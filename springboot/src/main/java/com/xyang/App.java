4058efcd49cfc8ce5328bb5a6d72110d276c3d0c
xyang4 <18610450436@163.com>
2016-09-21 23:53:26 +0800

    spring boot -- hello world

---
+++ springboot/src/main/java/com/xyang/App.java
@@ -0,0 +1,13 @@
+package com.xyang;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
