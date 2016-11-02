4d70be3a6f32ef72ac7d6e8367be6ce32e51549e
xyang4 <18610450436@163.com>
2016-11-02 23:14:02 +0800

    activemq深入学习 http://elim.iteye.com/blog/1900937

--- springmvc/src/main/java/com/xyang/mail/MailSenderService.java
+++ springmvc/src/main/java/com/xyang/mail/MailSenderService.java
@@ -10,7 +10,7 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
-@Service
+@Service("mailSenderService")
 public class MailSenderService {
 	@Autowired
 	private JavaMailSenderImpl javaMailSenderImpl;
@@ -23,11 +23,9 @@ public class MailSenderService {
 	 * @throws MessagingException
 	 * @throws UnsupportedEncodingException
 	 */
-	public MimeMessage createMimeMessage(MailBean mailBean)
-			throws MessagingException, UnsupportedEncodingException {
+	public MimeMessage createMimeMessage(MailBean mailBean) throws Exception {
 		MimeMessage mimeMessage = javaMailSenderImpl.createMimeMessage();
-		MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage,
-				true, "UTF-8");
+		MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage,true, "UTF-8");
 		messageHelper.setFrom(mailBean.getFrom(), mailBean.getFromName());
 		messageHelper.setSubject(mailBean.getSubject());
 		messageHelper.setTo(mailBean.getToEmails());
@@ -35,8 +33,7 @@ public class MailSenderService {
 		return mimeMessage;
 	}
 
-	public void sendMail(MailBean mailBean)
-			throws UnsupportedEncodingException, MessagingException {
+	public void sendMail(MailBean mailBean) throws Exception {
 		MimeMessage msg = createMimeMessage(mailBean);
 		javaMailSenderImpl.send(msg);
 	}
--- springmvc/src/main/java/com/xyang/mail/MailSenderTest.java
+++ springmvc/src/main/java/com/xyang/mail/MailSenderTest.java
@@ -7,18 +7,19 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = "classpath*:spring-mail.xml")
+@ContextConfiguration(locations = "classpath*:spring/spring-mail.xml")
 public class MailSenderTest {
 	@Autowired
 	private MailSenderService mailSenderService;
+	private static String from = "956172390@qq.com";
 
 	@Test
 	public void sendMailTest() throws Exception {
 		MailBean mailBean = new MailBean();
-		mailBean.setFrom("18610450436@163.com");
+		mailBean.setFrom(from);
 		mailBean.setFromName("xyang");
 		mailBean.setSubject("你好");
-		mailBean.setToEmails(new String[] { "956172390@qq.com","yangxudong@yitong111.com" });
+		mailBean.setToEmails(new String[] { "956172390@qq.com", "18610450436@163.com" });
 		mailBean.setContext("<a href='www.baidu.com'><font color='red'>fdsfdsf</font></a>");
 		mailSenderService.sendMail(mailBean);
 	}
