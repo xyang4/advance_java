41946e9cd9785f5f8e2eeabdce81ba7834cff4e0
xyang4 <18610450436@163.com>
2016-12-01 11:17:59 +0800

    spring 整合 mybatis时因与activemq内嵌日志冲突暂时没好办法解决，先搁置

--- springmvc/src/main/java/com/xyang/mybatis/service/impl/MsgServiceImpl.java
+++ springmvc/src/main/java/com/xyang/mybatis/service/impl/MsgServiceImpl.java
@@ -4,9 +4,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.xyang.mybatis.dao.MessageDao;
 import com.xyang.mybatis.entity.Message;
 import com.xyang.mybatis.entity.Page;
